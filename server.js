a69f4b18149f6eb0d9401d46b26a31175818ffdc
Apolline Diaz <apolline-diaz>
2024-03-04 14:51:58 +0100

    Installation serveur Node

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+  });
+
+
+
