7ee98210527d75777a7520715bd849b2f29a7547
SamSam <samuel.lavazais@hotmail.fr>
2024-03-06 00:20:00 +0100

    inject json data into product & productDetails component

--- frontend/src/components/product-page/product.jsx
+++ frontend/src/components/product-page/product.jsx
@@ -1,17 +1,23 @@
 import ProductDetails from "./productDetails.jsx";
 import BuyBtn from "./buyButton.jsx";
 
-function Product() {
+import data from "../../../../bouchon-data.json"
+
+
+function Product(productId) {
+
+    function getProductData(productId){
+        return data.products[productId]
+    }
+
     return (
-        <>
-            <div id="product">
-                <div id="placeholderCarousel"></div>
-                <div id="productMainSection">
-                    <ProductDetails />
-                    <BuyBtn />
-                </div>
+        <div id="product">
+            <div id="placeholderCarousel"></div>
+            <div id="productMainSection">
+                <ProductDetails productData={getProductData(1)} />
+                <BuyBtn />
             </div>
-        </>
+        </div>
     );
 }
 
--- frontend/src/components/product-page/productDetails.jsx
+++ frontend/src/components/product-page/productDetails.jsx
@@ -1,34 +1,35 @@
-function ProductDetails() {
+function ProductDetails({productData}) {
+    console.log(productData)
     return (
         <div id="productDetails">
             <div id="productName">MEUBLE VIEUX & MOCHE</div>
             <div className="detail">
                 <div className="detail-label">Prix</div>
-                <div className="detail-value">blabla</div>
+                <div className="detail-value">{productData.price} €</div>
             </div>
             <div className="detail">
                 <div className="detail-label">Category</div>
-                <div className="detail-value">blabla</div>
+                <div className="detail-value">{productData.category}</div>
             </div>
             <div className="detail">
                 <div className="detail-label">Description</div>
-                <div className="detail-value">blabla</div>
+                <div className="detail-value">{productData.description}</div>
             </div>
             <div className="detail">
                 <div className="detail-label">Dimensions</div>
-                <div className="detail-value">blabla</div>
+                <div className="detail-value">{productData.dimensions}</div>
             </div>
             <div className="detail">
                 <div className="detail-label">Couleur</div>
-                <div className="detail-value">blabla</div>
+                <div className="detail-value">{productData.color}</div>
             </div>
             <div className="detail">
                 <div className="detail-label">Matières</div>
-                <div className="detail-value">blabla</div>
+                <div className="detail-value">{productData.material}</div>
             </div>
             <div className="detail">
                 <div className="detail-label">Condition</div>
-                <div className="detail-value">blabla</div>
+                <div className="detail-value">{productData.condition}</div>
             </div>
         </div>
     );
