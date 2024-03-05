3d05749c6e1a675bf89310e406dda72060a1f6d2
SamSam <samuel.lavazais@hotmail.fr>
2024-03-05 23:14:08 +0100

    Create Components for Product Page : ProductDetails, Product, BuyBtn + css styles & grid display

---
+++ frontend/src/components/product-page/productDetails.jsx
@@ -0,0 +1,37 @@
+function ProductDetails() {
+    return (
+        <div id="productDetails">
+            <div id="productName">MEUBLE VIEUX & MOCHE</div>
+            <div className="detail">
+                <div className="detail-label">Prix</div>
+                <div className="detail-value">blabla</div>
+            </div>
+            <div className="detail">
+                <div className="detail-label">Category</div>
+                <div className="detail-value">blabla</div>
+            </div>
+            <div className="detail">
+                <div className="detail-label">Description</div>
+                <div className="detail-value">blabla</div>
+            </div>
+            <div className="detail">
+                <div className="detail-label">Dimensions</div>
+                <div className="detail-value">blabla</div>
+            </div>
+            <div className="detail">
+                <div className="detail-label">Couleur</div>
+                <div className="detail-value">blabla</div>
+            </div>
+            <div className="detail">
+                <div className="detail-label">Matières</div>
+                <div className="detail-value">blabla</div>
+            </div>
+            <div className="detail">
+                <div className="detail-label">Condition</div>
+                <div className="detail-value">blabla</div>
+            </div>
+        </div>
+    );
+}
+
+export default ProductDetails;
