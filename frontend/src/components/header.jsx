c3187836575ae83680da97d3b4c07a6b898bd923
SamSam <samuel.lavazais@hotmail.fr>
2024-03-05 15:54:20 +0100

    Header + create components

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <div id="header">
+            <div id="logo-h">Logo btn Home</div>
+            <div id="website-title">Site Title</div>
+            <div id="wishlist">wishlist</div>
+            <div id="user-account">user</div>
+        </div>
+    );
+}
+
+export default Header;
