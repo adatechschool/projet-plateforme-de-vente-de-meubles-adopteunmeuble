55fa88d28beb6d151dbd142366ddfe783dd5dbe4
SamSam <samuel.lavazais@hotmail.fr>
2024-03-05 23:51:33 +0100

    add header links

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
         <div id="header">
-            <div id="logo-h">Logo btn Home</div>
-            <div id="website-title">Site Title</div>
+            <div id="logo-h"><Link to="products/1">Logo btn Home</Link></div>
+            <div id="website-title"><Link to="products">Site Title</Link></div>
             <div id="wishlist">wishlist</div>
             <div id="user-account">user</div>
         </div>
