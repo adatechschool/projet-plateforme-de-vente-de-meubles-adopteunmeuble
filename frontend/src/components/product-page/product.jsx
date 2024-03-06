f19dad10318be87d53521622630a75bca7740b65
SamSam <samuel.lavazais@hotmail.fr>
2024-03-06 22:25:56 +0100

    inject data into ProductDetails

--- frontend/src/components/product-page/product.jsx
+++ frontend/src/components/product-page/product.jsx
@@ -1,13 +1,16 @@
 import ProductDetails from "./productDetails.jsx";
 import BuyBtn from "./buyButton.jsx";
+import data from '../../../../bouchon-data.json'
 
 function Product() {
+    let productData = data.products[2];
+
     return (
         <>
             <div id="product">
                 <div id="placeholderCarousel"></div>
                 <div id="productMainSection">
-                    <ProductDetails />
+                    <ProductDetails productData={productData} />
                     <BuyBtn />
                 </div>
             </div>
