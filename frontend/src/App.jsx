bb72f17c2d988e62da2a20a1a4e6e330207bf474
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2024-03-06 11:36:22 +0100

    carrousel V1 sans connection fetch bdd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Header from './components/header.jsx'
+import Carrousel from './components/product-page/carrousel';
 
 function App() {
   return (
     <>
       <Header/>
+      <div className="container-global">
+      <Carrousel/>
+      </div>
+
     </>
   )
 }
