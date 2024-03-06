87be363707d26dfa89484513f79bbee1d113bd68
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2024-03-06 16:59:46 +0100

    fix CSS bug for footer + add several modification to the Header

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -8,7 +8,6 @@ function Footer () {
             <a href="https://dae.gouv.nc/sites/default/files/atoms/files/modeletype_cgv_ccpc.pdf" target="_blank">Conditions Générales de Ventes</a>
             <a href="https://www.jimdo.com/fr/blog/comment-rediger-faq/#:~:text=Que%20signifie%20FAQ%20%3F,informations%20dont%20ils%20ont%20besoin." target="_blank">FAQ</a>
             <a href="https://www.oberlo.fr/blog/qui-sommes-nous-exemple" target="_blank">Qui Sommes Nous ?</a>
-            {/* <p>liens RS</p> */}
             <p>© Team Patate 2024</p>
         </div>
     )
--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -2,12 +2,11 @@ function Header() {
     return (
         <div id="header">
             <div id="logo-h">Logo btn Home</div>
-            <div id="website-title">Site Title</div>
+            <h2 id="website-title">Adopte un Meuble</h2>
             <div id="wishlist">wishlist</div>
             <div id="user-account">user</div>
         </div>
     );
 }
 
-
-export default Header
+export default Header;
